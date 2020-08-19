26696ee12df93f4100ee3d74aff869ccb86e6c48
vyar20 <vyar2005@gmail.com>
2020-08-19 17:28:43 +0700

    setup

---
+++ src/Redux/reducer.js
@@ -0,0 +1,13 @@
+import actions from './action';
+const initialState = {
+
+};
+
+const reducers = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default reducers
--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from "react-router-dom";
+import store from './Redux/store';
+import {Provider} from 'react-redux'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.Fragment>
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>
+</React.Fragment>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// unregister() to register() below. Note this comes with some pitfalls. Learn
+// more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
